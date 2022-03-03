0048277347cc0b45ed51e9dba1b7196d21d0e2ab
Welcome <tejaswini.bommireddy004@gmail.com>
2022-03-03 11:50:49 +0530

    comments,datatypes,variables

--- pythonbasics.py
+++ pythonbasics.py
@@ -1,1 +1,36 @@
-print("teju")
+#print("teju")
+#print("welcome to our college")
+#print("ba 7 kr degree college")
+#print("book price = 600")
+#print("my height is 4.9")
+#print("my name is teju")
+#a=10
+#b=20
+#if b > a:
+ #   print("b is greater than a")
+#else:
+#    print("b is not greater than a")
+
+# def sum(x,y):
+#     sum = x + y
+#     return sum
+# print(sum(5, 10))  #  it is a local variable
+# a = 20
+# def display():
+#     print("inside user-defined function:",a)
+#     display()
+#     print("outside the user-defined function",a)   #it is a global variable
+
+
+
+
+
+
+
+
+
+
+
+
+
+
