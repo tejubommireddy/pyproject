57fce650f6539277915c9c0219d52ead9af289ae
Welcome <tejaswini.bommireddy004@gmail.com>
2022-03-05 19:15:34 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,32 @@
+# a = 2
+# b = 3
+# print(a+b) # it is a addition operator
+# a = 10
+# b = 5
+# print(a-b) # it is a substraction operator
+# a = 2
+# b = 3
+# print(a*b) #it is a multiplicion operator
+# a = 10
+# b = 5
+# print(a/b)#it is a division operator
+# a = 20
+# a += 1
+# print(a) # increment operator
+# a = 20
+# a -= 5
+# print(a) # decrement operator
+# a = input("enter the first number: ")
+# b = input("enter the second number: ")
+# if a == b:
+#     print( "both inputs are equal")
+# else:
+#     print (" your input is not equal")
+# a=10
+# b=20
+# print("a>b is",a>b)
+# print("a>=b is",a>=b)
+# print("a<b is",a<b)
+# print("a<=b is",a<=b)
+# list1 = [10, 20, 30, 40 ]
+# print("smallest element is:" , min(list1))
