a13b91d4ec3bb0404bb2e52ce2d10bdf9ccd2053
Welcome <tejaswini.bommireddy004@gmail.com>
2022-03-08 17:46:05 +0530

    loops

---
+++ loops.py
@@ -0,0 +1,97 @@
+# for _ in range(10):
+#     print("bright it carrer")
+
+# to print 1 to 20 numbers using the while loop
+# i = 1
+# while(i<=20):
+#     print(i)
+#     i += 1
+
+# equal and notequal operators
+# a = 6
+# b = 7
+# print(a==b)# equal operator
+# print(a != b) #not rqual operators
+
+# to print even and odd numbers
+# num = int (input ("enter any number to test whether it is odd or even: "))
+# if (num % 2) == 0:
+#     print ("the number is even")
+# else:n
+#     print("the provided number is odd")\num1 = float(input("enter first number: ")
+
+# to print largest number among three variables
+# n1=int(input("first number:"))
+# n2=int(input("second number:"))
+# n3=int(input("third number:"))
+# if n1>n2 and n1>n3:
+#     print(n1,"is the largest number")
+# elif n2>n1 and n2>n3:
+#      print(n2,"is the largest number")
+# else:
+#     print(n3,"is the largest number")
+
+# to print 1 to 10 using while
+# #i = 1
+# while(i<=10):
+#     print(i)
+#     i += 1
+
+# to print even numbers between 1 to 20 using while
+# i = 1
+# while (i <= 20):
+#     if(i%2==0):
+#         print(i,end="")
+#     i+=1
+
+# to print armstrong number or not
+# num = int(input("enter a number: "))
+# sum = 0
+# temp = num
+# while temp > 0:
+#     digit = temp % 10
+#     sum += digit ** 3
+#     temp //= 10
+#     if num == sum:
+#         print(num,"is an armstrong number")
+#     else:
+#         print(num,"is not an armstrong number")
+# num = 11
+# if num > 1:
+#     for i in range(2, int(num/2)+1):
+#         if (num % i) == 0:
+#             print(num, "is not a prime number")
+#             break
+#         else:
+#             print(num, "is a prime number")
+# else:
+#         print(num, "is not a prime number")
+
+# palindrome or not
+# def ispalindrome(s):
+#     return s == s[::-1]
+# s = "malyalam"
+# ans = ispalindrome(s)
+# if ans:
+#     print("yes")
+# else:
+#     print("no")
+
+# even or odd
+# num = int(input("enter a number: "))
+# if (num % 2) == 0:
+#     print("{0} is even".format(num))
+# else:
+#     print("{0} is odd".format(num))
+
+
+#  gender programm
+# gender = input("what is your gender? ")
+# if 'male' == gender:
+#     print("you are a boy!")
+# else:
+#     print("you are a girl")
+
+
+
+
