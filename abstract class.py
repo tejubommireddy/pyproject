d8c8013a2907d3bef8bceb2f32feb0dccc34117c
Welcome <tejaswini.bommireddy004@gmail.com>
2022-04-17 14:45:34 +0530

    abstract methods

---
+++ abstract class.py
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+
+
+
+
+# create an abstract class
+# from abc import ABC, abstractmethod
+# class polygon(ABC):
+#     @abstractmethod
+#     def noofsides(self):
+#         pass
+# class triangle(polygon):
+#     def noofsides(self):
+#         print("i have 3 sides")
+# class pentagon(polygon):
+#     def noofsides(self):
+#         print("i have 5 sides")
+# class hexagon(polygon):
+#      def noofsides(self):
+#         print("i have 6 sides")
+# class quadrilateral(polygon):
+#     def noofsides(self):
+#         print("i have 4 sides")
+# R = triangle()
+# R.noofsides()
+# K = quadrilateral()
+# K.noofsides()
+# R = pentagon()
+# R.noofsides()
+# K = hexagon()
+# K.noofsides()
+
+
+# create sub class to the abstract class
+# from abc import ABC, abstractmethod
+# class AbstractClassExample(ABC):
+#     @abstractmethod
+#     def do_something(self):
+#         print("some implementation!")
+# class AnotherSubClass(AbstractClassExample):
+#      def do_something(self):
+#          super().do_something()
+#          print("the enrichment from anothersubclass")
+# x = AnotherSubClass()
+# x.do_something()
+
+
+
+
+
+-
